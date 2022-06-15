368fa4beec048d7739fa0c230ff86382706050a0
Joe <socmedia.dump@protonmail.ch>
2022-06-15 14:58:14 -0400

    Creation of Index method

--- ecommerce/app/Http/Controllers/Demo/DemoController.php
+++ ecommerce/app/Http/Controllers/Demo/DemoController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {
-    //
+    // this is the "Index" method from web.php
+    public function Index() {
+        return view('about');
+    } // end method
 }
