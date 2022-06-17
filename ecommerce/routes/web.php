35683e5caf8535de2935bb097cdc40a0e5b8e388
Joe <socmedia.dump@protonmail.ch>
2022-06-17 09:28:57 -0400

    Add redirect via middlware CheckAge, update named route

--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 });
 
 Route::controller(DemoController::class)->group(function() { // multiple routes put into a group
-    Route::get('/about', 'Index'); // load the "Index" method, URL route
+    Route::get('/about', 'Index')->name('about.page')->middleware('check'); // load the "Index" method, URL route
     Route::get('/contact', 'ContactMethod')->name('contact.page'); // named route
 });
 
